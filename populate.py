afce3a3d3c0fbd1de97da2b911da846c7e70090f
Pooria Soltani <pooria.ms@gmail.com>
2019-06-08 22:27:20 +0430

    Some refinements to the Database class (wrapper)

--- populate.py
+++ populate.py
@@ -23,41 +23,41 @@ from wrapper import Database, country
 #         'paper_keyword': {'order': 3, 'id': ['paper_id', 'keyword_id']},
 #     }
 
-countries = []
-with io.open('data\\Countries.csv', 'r', encoding='utf-8-sig') as csvFile:
-    reader = csv.DictReader(csvFile)
-    for row in reader:
-       countries.append(row)
+# countries = []
+# with io.open('data\\Countries.csv', 'r', encoding='utf-8-sig') as csvFile:
+#     reader = csv.DictReader(csvFile)
+#     for row in reader:
+#        countries.append(row)
 
-faculties = []
-with io.open('data\\faculties.csv', 'r', encoding='utf-8-sig') as csvFile:
-    reader = csv.DictReader(csvFile)
-    for row in reader:
-        if row['Scopus']:
-            row['Scopus'] = list(map(int, row['Scopus'].split(',')))
-        faculties.append(row)
+# faculties = []
+# with io.open('data\\faculties.csv', 'r', encoding='utf-8-sig') as csvFile:
+#     reader = csv.DictReader(csvFile)
+#     for row in reader:
+#         if row['Scopus']:
+#             row['Scopus'] = list(map(int, row['Scopus'].split(',')))
+#         faculties.append(row)
 
-asjc = []
-with io.open('data\\ASJC Codes.csv', 'r', encoding='utf-8-sig') as csvFile:
-    reader = csv.DictReader(csvFile)
-    for row in reader:
-        asjc.append(row)
+# asjc = []
+# with io.open('data\\ASJC Codes.csv', 'r', encoding='utf-8-sig') as csvFile:
+#     reader = csv.DictReader(csvFile)
+#     for row in reader:
+#         asjc.append(row)
 
-sources = []
-with io.open('data\\Scopus Sources.csv', 'r', encoding='utf-8-sig') as csvFile:
-    reader = csv.DictReader(csvFile)
-    to_be_removed = [
-        'Active', 'Discontinued', 'Coverage', '2016 CiteScore', '2017 CiteScore', '2018 CiteScore',
-        'Medline-sourced', 'Open Access', 'Articles in Press Included', 'Added to list April 2019',
-        'Title history indication', 'Related title to title history indication', 'Other related title 1',
-        'Other related title 2', 'Other related title 3', 'Publisher imprints grouped to main Publisher',
-    ]
-    for row in reader:
-        for col in to_be_removed:
-            row.pop(col, None)
-        row['ASJC'] = [int(code)
-                       for code in row['ASJC'].split(';') if code != '']
-        sources.append(row)
+# sources = []
+# with io.open('data\\Scopus Sources.csv', 'r', encoding='utf-8-sig') as csvFile:
+#     reader = csv.DictReader(csvFile)
+#     to_be_removed = [
+#         'Active', 'Discontinued', 'Coverage', '2016 CiteScore', '2017 CiteScore', '2018 CiteScore',
+#         'Medline-sourced', 'Open Access', 'Articles in Press Included', 'Added to list April 2019',
+#         'Title history indication', 'Related title to title history indication', 'Other related title 1',
+#         'Other related title 2', 'Other related title 3', 'Publisher imprints grouped to main Publisher',
+#     ]
+#     for row in reader:
+#         for col in to_be_removed:
+#             row.pop(col, None)
+#         row['ASJC'] = [int(code)
+#                        for code in row['ASJC'].split(';') if code != '']
+#         sources.append(row)
 
 d2 = Database(config=client, db_name='scopus')
 
@@ -84,11 +84,40 @@ d2 = Database(config=client, db_name='scopus')
 #     server_response = d2._insert_one('country', country_info)
 
 
-for cnt, source in enumerate(sources[:100]):
-    source_info = {
-        'source_id_scp': source['Source ID'],
-        'publisher': source['Publisher'],
-        'country': d2._read('country', search={'name': {'value': country(source['Country']), 'operator': '='}})[-1][0],
-    }
-    print(cnt)
-    print(source_info)
+# for cnt, source in enumerate(sources[:100]):
+#     source_info = {
+#         'source_id_scp': source['Source ID'],
+#         'publisher': source['Publisher'],
+#         'country': d2._read(
+#             'country',
+#             search={'name': {'value': country(source['Country']), 'operator': '='}}
+#         )[-1][0],
+#     }
+#     print(cnt)
+#     print(source_info)
+# select = '*'
+# table_name = 'subject'
+# search = {
+#     'type': {'value': 'ar', 'operator': '='},
+#     'source_id': {'value': 1234, 'operator': '='}
+# }
+# data = [
+#     {
+#         'asjc_code': 9999,
+#         'top': 'tttop',
+#         'middle': 'tmmiddle',
+#         'low': 'ttllow'
+#     },
+#     {
+#         'asjc_code': 1111,
+#         'top': 'ztttop',
+#         'middle': 'ztmmiddle',
+#         'low': 'ztllow'
+#     }
+# ]
+# server_response = d2._insert_many(table_name=table_name, data=data)
+# print(server_response)
+# for k, v in server_response.items():
+#     print(k)
+#     print(v)
+#     print()
