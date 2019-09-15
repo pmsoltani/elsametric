92e1033382d295e14d8234b3f5992f378c668e72
Pooria Soltani <pooria.ms@gmail.com>
2019-09-15 10:41:43 +0430

    Removed the magic number from the token_generator function and set it as a default.

--- elsametric/db_classes/base.py
+++ elsametric/db_classes/base.py
@@ -25,7 +25,8 @@ Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
+
 # Helper function to generate 8-bytes tokens for 'id_front' columns
 # The text is Base64 encoded, so each byte is 1.3 chars (Total 11 chars).
-def token_generator():
-    return secrets.token_urlsafe(8)
+def token_generator(nbytes=8):
+    return secrets.token_urlsafe(nbytes)
