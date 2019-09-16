08358618c5c58591441277d82beb7036d2535da6
Pooria Soltani <pooria.ms@gmail.com>
2019-09-16 22:47:11 +0430

    Changed the way config is read, to reflect the new changes in the structure of 'config.json'.

--- elsametric/__init__.py
+++ elsametric/__init__.py
@@ -6,4 +6,4 @@ config_path = os.path.join(os.getcwd(), 'config.json')
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
 
-config = config['database']
+config = config['database']['startup']
