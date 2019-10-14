35bced0148359348c62723171b43bd5c00bc4584
Pooria Soltani <pooria.ms@gmail.com>
2019-10-14 22:17:43 +0330

    Closes #24

--- elsametric/models/base.py
+++ elsametric/models/base.py
@@ -4,13 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .. import config
+from .. import engine_uri
 
-db_user = config['MySQL User']
-db_pass = config['MySQL Pass']
-db_host = config['MySQL Host']
-db_schema = config['MySQL Schema']
-engine_uri = f'mysql+mysqlconnector://{db_user}:{db_pass}@{db_host}/{db_schema}'
 
 engine = create_engine(engine_uri)
 Session = sessionmaker(bind=engine)
