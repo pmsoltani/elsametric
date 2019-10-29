pmsoltani/elsametric

commit f1460dcdd3e4f975bdfd12e62918a0cd4593a575

Imported column types from 'sqlalchemy.types' instead of MySQL specific column types. Added check constraints for unsigned values.

elsametric/models/associations.py
@@ -1,31 +1,32 @@
-from sqlalchemy import Column, Table, ForeignKey, ForeignKeyConstraint
+from sqlalchemy import (
+    Column,
+    CheckConstraint,
+    ForeignKey,
+    ForeignKeyConstraint,
+    Table,
+)
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import BIGINT, INTEGER, SMALLINT
+from sqlalchemy.types import BIGINT, INTEGER, SMALLINT
 
 from .base import Base
 
 Source_Subject = Table(
     'source_subject', Base.metadata,
-    Column('source_id', INTEGER(unsigned=True),
-           ForeignKey('source.id'), primary_key=True),
-    Column('subject_id', INTEGER(unsigned=True),
-           ForeignKey('subject.id'), primary_key=True)
+    Column('source_id', INTEGER, ForeignKey('source.id'), primary_key=True),
+    Column('subject_id', INTEGER, ForeignKey('subject.id'), primary_key=True)
 )
 
 Paper_Keyword = Table(
     'paper_keyword', Base.metadata,
-    Column('paper_id', INTEGER(unsigned=True),
-           ForeignKey('paper.id'), primary_key=True),
-    Column('keyword_id', BIGINT(unsigned=True),
-           ForeignKey('keyword.id'), primary_key=True)
+    Column('paper_id', INTEGER, ForeignKey('paper.id'), primary_key=True),
+    Column('keyword_id', BIGINT, ForeignKey('keyword.id'), primary_key=True)
 )
 
 Author_Department = Table(
     'author_department', Base.metadata,
-    Column('author_id', INTEGER(unsigned=True),
-           ForeignKey('author.id'), primary_key=True),
-    Column('department_id', INTEGER(unsigned=True), primary_key=True),
-    Column('institution_id', INTEGER(unsigned=True), primary_key=True),
+    Column('author_id', INTEGER, ForeignKey('author.id'), primary_key=True),
+    Column('department_id', INTEGER, primary_key=True),
+    Column('institution_id', INTEGER, primary_key=True),
     ForeignKeyConstraint(
         ('department_id', 'institution_id'),
         ('department.id', 'department.institution_id'),
@@ -35,12 +36,13 @@ Author_Department = Table(
 
 class Paper_Author(Base):
     __tablename__ = 'paper_author'
+    __table_args__ = (
+        CheckConstraint('author_no >= 0', name='author_no_unsigned'),
+    )
 
-    paper_id = Column(
-        INTEGER(unsigned=True), ForeignKey('paper.id'), primary_key=True)
-    author_id = Column(
-        INTEGER(unsigned=True), ForeignKey('author.id'), primary_key=True)
-    author_no = Column(SMALLINT(unsigned=True), nullable=False)
+    paper_id = Column(INTEGER, ForeignKey('paper.id'), primary_key=True)
+    author_id = Column(INTEGER, ForeignKey('author.id'), primary_key=True)
+    author_no = Column(SMALLINT, nullable=False)
 
     # Relationships
     paper = relationship('Paper', back_populates='authors')

elsametric/models/author.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from typing import Mapping, Set
 
-from sqlalchemy import Column, CheckConstraint, text
+from sqlalchemy import CheckConstraint, Column, DDL, event, text
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import \
-    BIGINT, CHAR, DATETIME, ENUM, INTEGER, VARCHAR
+from sqlalchemy.types import BIGINT, CHAR, DateTime, Enum, INTEGER, VARCHAR
 
 from .base import Base, token_generator, VARCHAR_COLUMN_LENGTH
 from .associations import Author_Department, Paper_Author
@@ -24,41 +23,39 @@ class Author(Base):
             )''',
             name='ck_author_google_scholar'
         ),
+        CheckConstraint('''sex IN ('m', 'f')''', name='gender_types'),
+        # CheckConstraint('id >= 0', name='author_id_unsigned'),
+        CheckConstraint('id_scp >= 0', name='author_id_scp_unsigned'),
+        CheckConstraint('h_index_gsc >= 0', name='h_index_gsc_unsigned'),
+        CheckConstraint('i10_index_gsc >= 0', name='i10_index_gsc_unsigned'),
     )
 
-    id = Column(
-        INTEGER(unsigned=True),
-        primary_key=True, autoincrement=True, nullable=False
-    )
-    id_scp = Column(BIGINT(unsigned=True), nullable=False, unique=True)
-    id_gsc = Column(VARCHAR(12), nullable=True, unique=True)
-    id_institution = Column(VARCHAR(45), nullable=True)
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    id_scp = Column(BIGINT, nullable=False, unique=True)
+    id_gsc = Column(VARCHAR(12), unique=True)
+    id_institution = Column(VARCHAR(45))
     id_frontend = Column(
         VARCHAR(VARCHAR_COLUMN_LENGTH), nullable=False, unique=True)
-    first = Column(VARCHAR(45), nullable=True)
-    middle = Column(VARCHAR(45), nullable=True)
-    last = Column(VARCHAR(45), nullable=True)
-    initials = Column(VARCHAR(45), nullable=True)
-    first_pref = Column(VARCHAR(45), nullable=True)
-    middle_pref = Column(VARCHAR(45), nullable=True)
-    last_pref = Column(VARCHAR(45), nullable=True)
-    initials_pref = Column(VARCHAR(45), nullable=True)
-    first_fa = Column(VARCHAR(45), nullable=True)
-    last_fa = Column(VARCHAR(45), nullable=True)
-    sex = Column(CHAR(1), ENUM('m', 'f'), nullable=True)
-    type = Column(VARCHAR(45), nullable=True)
-    rank = Column(VARCHAR(45), nullable=True)
-    h_index_gsc = Column(INTEGER(unsigned=True), nullable=True)
-    i10_index_gsc = Column(INTEGER(unsigned=True), nullable=True)
-    retrieval_time_gsc = Column(DATETIME(), nullable=True)
+    first = Column(VARCHAR(45))
+    middle = Column(VARCHAR(45))
+    last = Column(VARCHAR(45))
+    initials = Column(VARCHAR(45))
+    first_pref = Column(VARCHAR(45))
+    middle_pref = Column(VARCHAR(45))
+    last_pref = Column(VARCHAR(45))
+    initials_pref = Column(VARCHAR(45))
+    first_fa = Column(VARCHAR(45))
+    last_fa = Column(VARCHAR(45))
+    sex = Column(CHAR(1), Enum('m', 'f', name='gender_types'))
+    type = Column(VARCHAR(45))
+    rank = Column(VARCHAR(45))
+    h_index_gsc = Column(INTEGER)
+    i10_index_gsc = Column(INTEGER)
+    retrieval_time_gsc = Column(DateTime())
     create_time = Column(
-        DATETIME(), nullable=False,
-        server_default=text('CURRENT_TIMESTAMP')
-    )
+        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DATETIME(), nullable=False,
-        server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
-    )
+        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
 
     # Relationships
     papers = relationship('Paper_Author', back_populates='author')
@@ -265,3 +262,23 @@ class Author(Base):
                 continue
 
         return self._funds
+
+
+# update_time_trigger = DDL(
+#     '''
+#     CREATE OR REPLACE FUNCTION set_update_time()
+#     RETURNS TRIGGER AS $$
+#     BEGIN
+#         NEW.update_time = now();
+#         RETURN NEW;
+#     END;
+#     $$ language 'plpgsql';
+
+#     CREATE TRIGGER author_update_time
+#         BEFORE UPDATE ON author
+#         FOR EACH ROW
+#         EXECUTE PROCEDURE  set_update_time();
+#     '''
+# )
+
+# event.listen(Author.__table__, 'after_create', update_time_trigger)

elsametric/models/author_profile.py
@@ -1,21 +1,18 @@
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import CheckConstraint, Column, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import BIGINT, INTEGER, VARCHAR
+from sqlalchemy.types import BIGINT, INTEGER, SMALLINT, VARCHAR
 
 from .base import Base
 
 
 class Author_Profile(Base):
     __tablename__ = 'author_profile'
+    # __table_args__ = (
+    #     CheckConstraint('id >= 0', name='author_profile_id_unsigned'),
+    # )
 
-    id = Column(
-        BIGINT(unsigned=True),
-        primary_key=True, autoincrement=True, nullable=False
-    )
-    author_id = Column(
-        INTEGER(unsigned=True),
-        ForeignKey('author.id'), primary_key=True, nullable=False
-    )
+    id = Column(BIGINT, primary_key=True, autoincrement=True)
+    author_id = Column(INTEGER, ForeignKey('author.id'), primary_key=True)
     address = Column(VARCHAR(256), nullable=False, unique=True)
     type = Column(VARCHAR(45), nullable=False)
 

elsametric/models/country.py
@@ -1,22 +1,22 @@
-from sqlalchemy import Column
+from sqlalchemy import CheckConstraint, Column
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import INTEGER, VARCHAR
+from sqlalchemy.types import INTEGER, VARCHAR
 
 from .base import Base
 
 
 class Country(Base):
     __tablename__ = 'country'
+    # __table_args__ = (
+    #     CheckConstraint('id >= 0', name='country_id_unsigned'),
+    # )
 
-    id = Column(
-        INTEGER(unsigned=True),
-        primary_key=True, autoincrement=True, nullable=False
-    )
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
     name = Column(VARCHAR(45), nullable=False, unique=True)
-    name_fa = Column(VARCHAR(45), nullable=True)
+    name_fa = Column(VARCHAR(45))
     domain = Column(VARCHAR(2), nullable=False, unique=True)
     region = Column(VARCHAR(10), nullable=False)
-    sub_region = Column(VARCHAR(45), nullable=True)
+    sub_region = Column(VARCHAR(45))
 
     # Relationships
     sources = relationship('Source', back_populates='country')

commit 06bc1c1b9be4fa0b764e08667581e21da1600f8e

Fixed the problems caused by f1460dc. Closes #50.

elsametric/__init__.py
@@ -14,11 +14,16 @@ config = config['database']
 
 TOKEN_BYTES = config['token_bytes']
 
-DB_USER = config['startup']['mysql_user']
-DB_PASS = config['startup']['mysql_pass']
-DB_HOST = config['startup']['mysql_host']
-DB_NAME = config['startup']['mysql_schema']
-ENGINE_URI = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+DIALECT = config['startup']['dialect']
+assert DIALECT in ('mysql', 'postgresql')
+
+DB_DRIVER = config['startup'][DIALECT]['driver']
+DB_USER = config['startup'][DIALECT]['user']
+DB_PASS = config['startup'][DIALECT]['pass']
+DB_HOST = config['startup'][DIALECT]['host']
+DB_NAME = config['startup'][DIALECT]['schema']
+
+ENGINE_URI = f'{DIALECT}+{DB_DRIVER}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 
 if not database_exists(ENGINE_URI):
-    create_database(ENGINE_URI, encoding='utf8mb4')
+    create_database(ENGINE_URI, encoding='utf8')

elsametric/models/associations.py
@@ -8,7 +8,7 @@ from sqlalchemy import (
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, INTEGER, SMALLINT
 
-from .base import Base
+from .base import Base, DIALECT
 
 Source_Subject = Table(
     'source_subject', Base.metadata,

elsametric/models/author.py
@@ -5,7 +5,13 @@ from sqlalchemy import CheckConstraint, Column, DDL, event, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, CHAR, DateTime, Enum, INTEGER, VARCHAR
 
-from .base import Base, token_generator, VARCHAR_COLUMN_LENGTH
+from .base import (
+    Base,
+    DIALECT,
+    token_generator,
+    UPDATE_TIME_DEFAULT,
+    VARCHAR_COLUMN_LENGTH
+)
 from .associations import Author_Department, Paper_Author
 from .country import Country
 from .institution import Institution
@@ -17,6 +23,13 @@ class Author(Base):
     __tablename__ = 'author'
     __table_args__ = (
         CheckConstraint(
+            'id >= 0',
+            name='author_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+        CheckConstraint('id_scp >= 0', name='author_id_scp_unsigned'),
+        CheckConstraint('h_index_gsc >= 0', name='h_index_gsc_unsigned'),
+        CheckConstraint('i10_index_gsc >= 0', name='i10_index_gsc_unsigned'),
+        CheckConstraint(
             '''NOT(
                 (NOT(h_index_gsc IS NULL) OR NOT(i10_index_gsc IS NULL)) AND
                 (retrieval_time_gsc IS NULL)
@@ -24,10 +37,6 @@ class Author(Base):
             name='ck_author_google_scholar'
         ),
         CheckConstraint('''sex IN ('m', 'f')''', name='gender_types'),
-        # CheckConstraint('id >= 0', name='author_id_unsigned'),
-        CheckConstraint('id_scp >= 0', name='author_id_scp_unsigned'),
-        CheckConstraint('h_index_gsc >= 0', name='h_index_gsc_unsigned'),
-        CheckConstraint('i10_index_gsc >= 0', name='i10_index_gsc_unsigned'),
     )
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
@@ -55,7 +64,7 @@ class Author(Base):
     create_time = Column(
         DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+        DateTime(), nullable=False, server_default=text(UPDATE_TIME_DEFAULT))
 
     # Relationships
     papers = relationship('Paper_Author', back_populates='author')
@@ -264,21 +273,22 @@ class Author(Base):
         return self._funds
 
 
-# update_time_trigger = DDL(
-#     '''
-#     CREATE OR REPLACE FUNCTION set_update_time()
-#     RETURNS TRIGGER AS $$
-#     BEGIN
-#         NEW.update_time = now();
-#         RETURN NEW;
-#     END;
-#     $$ language 'plpgsql';
+if DIALECT == "postgresql":
+    update_time_trigger = DDL(
+        '''
+        CREATE OR REPLACE FUNCTION set_update_time()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_time = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
 
-#     CREATE TRIGGER author_update_time
-#         BEFORE UPDATE ON author
-#         FOR EACH ROW
-#         EXECUTE PROCEDURE  set_update_time();
-#     '''
-# )
+        CREATE TRIGGER author_update_time
+            BEFORE UPDATE ON author
+            FOR EACH ROW
+            EXECUTE PROCEDURE  set_update_time();
+        '''
+    )
 
-# event.listen(Author.__table__, 'after_create', update_time_trigger)
+    event.listen(Author.__table__, 'after_create', update_time_trigger)

elsametric/models/author_profile.py
@@ -2,14 +2,17 @@ from sqlalchemy import CheckConstraint, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, INTEGER, SMALLINT, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 
 
 class Author_Profile(Base):
     __tablename__ = 'author_profile'
-    # __table_args__ = (
-    #     CheckConstraint('id >= 0', name='author_profile_id_unsigned'),
-    # )
+    __table_args__ = (
+        CheckConstraint(
+            'id >= 0',
+            name='author_profile_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+    )
 
     id = Column(BIGINT, primary_key=True, autoincrement=True)
     author_id = Column(INTEGER, ForeignKey('author.id'), primary_key=True)

elsametric/models/base.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .. import ENGINE_URI, TOKEN_BYTES
+from .. import DIALECT, ENGINE_URI, TOKEN_BYTES
 
 
 engine = create_engine(ENGINE_URI)
@@ -16,6 +16,13 @@ Base = declarative_base()
 # Helper function to generate tokens with 'TOKEN_BYTES' for 'id_front' columns
 def token_generator(nbytes: int = TOKEN_BYTES) -> str:
     return secrets.token_urlsafe(nbytes)
+
+
+# Set the 'server_default' parameter of the 'update_time' columns that is used
+# in multiple classes, such as Paper, Author, and Source
+UPDATE_TIME_DEFAULT = 'CURRENT_TIMESTAMP'
+if DIALECT == "mysql":
+    UPDATE_TIME_DEFAULT = 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
 
 
 # Import the following constant in modules employing `token_generator`. Note:

elsametric/models/country.py
@@ -2,14 +2,17 @@ from sqlalchemy import CheckConstraint, Column
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import INTEGER, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 
 
 class Country(Base):
     __tablename__ = 'country'
-    # __table_args__ = (
-    #     CheckConstraint('id >= 0', name='country_id_unsigned'),
-    # )
+    __table_args__ = (
+        CheckConstraint(
+            'id >= 0',
+            name='country_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+    )
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
     name = Column(VARCHAR(45), nullable=False, unique=True)

elsametric/models/department.py
@@ -5,7 +5,13 @@ from sqlalchemy import CheckConstraint, Column, DDL, event, ForeignKey, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import DateTime, DECIMAL, INTEGER, VARCHAR
 
-from .base import Base, token_generator, VARCHAR_COLUMN_LENGTH
+from .base import (
+    Base,
+    DIALECT,
+    token_generator,
+    UPDATE_TIME_DEFAULT,
+    VARCHAR_COLUMN_LENGTH
+)
 from .author import Author
 from .associations import Author_Department
 from .source import Source
@@ -14,9 +20,12 @@ from .subject import Subject
 
 class Department(Base):
     __tablename__ = 'department'
-    # __table_args__ = (
-    #     CheckConstraint('id >= 0', name='department_id_unsigned'),
-    # )
+    __table_args__ = (
+        CheckConstraint(
+            'id >= 0',
+            name='department_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+    )
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
     institution_id = Column(
@@ -33,7 +42,7 @@ class Department(Base):
     create_time = Column(
         DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+        DateTime(), nullable=False, server_default=text(UPDATE_TIME_DEFAULT))
 
     # Relationships
     institution = relationship('Institution', back_populates='departments')
@@ -204,21 +213,22 @@ class Department(Base):
         return self._funds
 
 
-# update_time_trigger = DDL(
-#     '''
-#     CREATE OR REPLACE FUNCTION set_update_time()
-#     RETURNS TRIGGER AS $$
-#     BEGIN
-#         NEW.update_time = now();
-#         RETURN NEW;
-#     END;
-#     $$ language 'plpgsql';
+if DIALECT == "postgresql":
+    update_time_trigger = DDL(
+        '''
+        CREATE OR REPLACE FUNCTION set_update_time()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_time = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
 
-#     CREATE TRIGGER department_update_time
-#         BEFORE UPDATE ON department
-#         FOR EACH ROW
-#         EXECUTE PROCEDURE  set_update_time();
-#     '''
-# )
+        CREATE TRIGGER department_update_time
+            BEFORE UPDATE ON department
+            FOR EACH ROW
+            EXECUTE PROCEDURE  set_update_time();
+        '''
+    )
 
-# event.listen(Department.__table__, 'after_create', update_time_trigger)
+    event.listen(Department.__table__, 'after_create', update_time_trigger)

elsametric/models/fund.py
@@ -2,17 +2,20 @@ from sqlalchemy import CheckConstraint, Column, UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 
 
 class Fund(Base):
     __tablename__ = 'fund'
     __table_args__ = (
         CheckConstraint(
+            'id >= 0',
+            name='fund_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+        CheckConstraint(
             'NOT(id_scp IS NULL AND agency IS NULL)',
             name='ck_fund_idscp_agency'
         ),
-        # CheckConstraint('id >= 0', name='fund_id_unsigned'),
         UniqueConstraint('id_scp', 'agency', name='uq_fund_idscp_agency'),
     )
 

elsametric/models/institution.py
@@ -4,13 +4,22 @@ from sqlalchemy import CheckConstraint, Column, DDL, event, ForeignKey, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, DateTime, DECIMAL, INTEGER, VARCHAR
 
-from .base import Base, token_generator, VARCHAR_COLUMN_LENGTH
+from .base import (
+    Base,
+    DIALECT,
+    token_generator,
+    UPDATE_TIME_DEFAULT,
+    VARCHAR_COLUMN_LENGTH
+)
 
 
 class Institution(Base):
     __tablename__ = 'institution'
     __table_args__ = (
-        # CheckConstraint('id >= 0', name='institution_id_unsigned'),
+        CheckConstraint(
+            'id >= 0',
+            name='institution_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
         CheckConstraint('id_scp >= 0', name='institution_id_scp_unsigned'),
     )
 
@@ -30,7 +39,7 @@ class Institution(Base):
     create_time = Column(
         DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+        DateTime(), nullable=False, server_default=text(UPDATE_TIME_DEFAULT))
 
     # Relationships
     country = relationship('Country', back_populates='institutions')
@@ -65,21 +74,22 @@ class Institution(Base):
         return f'{self.id_scp}: {self.name[:max_len-3]}...'
 
 
-# update_time_trigger = DDL(
-#     '''
-#     CREATE OR REPLACE FUNCTION set_update_time()
-#     RETURNS TRIGGER AS $$
-#     BEGIN
-#         NEW.update_time = now();
-#         RETURN NEW;
-#     END;
-#     $$ language 'plpgsql';
+if DIALECT == "postgresql":
+    update_time_trigger = DDL(
+        '''
+        CREATE OR REPLACE FUNCTION set_update_time()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_time = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
 
-#     CREATE TRIGGER institution_update_time
-#         BEFORE UPDATE ON institution
-#         FOR EACH ROW
-#         EXECUTE PROCEDURE  set_update_time();
-#     '''
-# )
+        CREATE TRIGGER institution_update_time
+            BEFORE UPDATE ON institution
+            FOR EACH ROW
+            EXECUTE PROCEDURE  set_update_time();
+        '''
+    )
 
-# event.listen(Institution.__table__, 'after_create', update_time_trigger)
+    event.listen(Institution.__table__, 'after_create', update_time_trigger)

elsametric/models/keyword_.py
@@ -2,15 +2,18 @@ from sqlalchemy import CheckConstraint, Column
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 from .associations import Paper_Keyword
 
 
 class Keyword(Base):
     __tablename__ = 'keyword'
-    # __table_args__ = (
-    #     CheckConstraint('id >= 0', name='keyword_id_unsigned'),
-    # )
+    __table_args__ = (
+        CheckConstraint(
+            'id >= 0',
+            name='keyword_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
+    )
 
     id = Column(BIGINT, primary_key=True, autoincrement=True)
     keyword = Column(VARCHAR(256), nullable=False, unique=True)

elsametric/models/paper.py
@@ -13,14 +13,17 @@ from sqlalchemy.types import (
     VARCHAR
 )
 
-from .base import Base
+from .base import Base, DIALECT, UPDATE_TIME_DEFAULT
 from .associations import Paper_Keyword, Paper_Author
 
 
 class Paper(Base):
     __tablename__ = 'paper'
     __table_args__ = (
-        # CheckConstraint('id >= 0', name='paper_id_unsigned'),
+        CheckConstraint(
+            'id >= 0',
+            name='paper_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
         CheckConstraint('id_scp >= 0', name='paper_id_scp_unsigned'),
         CheckConstraint('total_author >= 0', name='total_author_unsigned'),
         CheckConstraint('cited_cnt >= 0', name='cited_cnt_unsigned'),
@@ -52,7 +55,7 @@ class Paper(Base):
     create_time = Column(
         DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+        DateTime(), nullable=False, server_default=text(UPDATE_TIME_DEFAULT))
 
     # Relationships
     fund = relationship('Fund', back_populates='papers')
@@ -103,21 +106,22 @@ class Paper(Base):
         return self.date.year
 
 
-# update_time_trigger = DDL(
-#     '''
-#     CREATE OR REPLACE FUNCTION set_update_time()
-#     RETURNS TRIGGER AS $$
-#     BEGIN
-#         NEW.update_time = now();
-#         RETURN NEW;
-#     END;
-#     $$ language 'plpgsql';
+if DIALECT == "postgresql":
+    update_time_trigger = DDL(
+        '''
+        CREATE OR REPLACE FUNCTION set_update_time()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_time = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
 
-#     CREATE TRIGGER paper_update_time
-#         BEFORE UPDATE ON paper
-#         FOR EACH ROW
-#         EXECUTE PROCEDURE  set_update_time();
-#     '''
-# )
+        CREATE TRIGGER paper_update_time
+            BEFORE UPDATE ON paper
+            FOR EACH ROW
+            EXECUTE PROCEDURE  set_update_time();
+        '''
+    )
 
-# event.listen(Paper.__table__, 'after_create', update_time_trigger)
+    event.listen(Paper.__table__, 'after_create', update_time_trigger)

elsametric/models/source.py
@@ -4,14 +4,17 @@ from sqlalchemy import CheckConstraint, Column, DDL, event, ForeignKey, text
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import BIGINT, DateTime, INTEGER, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT, UPDATE_TIME_DEFAULT
 from .associations import Source_Subject
 
 
 class Source(Base):
     __tablename__ = 'source'
     __table_args__ = (
-        # CheckConstraint('id >= 0', name='source_id_unsigned'),
+        CheckConstraint(
+            'id >= 0',
+            name='source_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
         CheckConstraint('id_scp >= 0', name='source_id_scp_unsigned'),
     )
 
@@ -28,7 +31,7 @@ class Source(Base):
     create_time = Column(
         DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     update_time = Column(
-        DateTime(), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+        DateTime(), nullable=False, server_default=text(UPDATE_TIME_DEFAULT))
 
     # Relationships
     country = relationship('Country', back_populates='sources')
@@ -62,21 +65,22 @@ class Source(Base):
         return f'{self.id_scp}: {self.title[:max_len-3]}...; Type: {self.type}'
 
 
-# update_time_trigger = DDL(
-#     '''
-#     CREATE OR REPLACE FUNCTION set_update_time()
-#     RETURNS TRIGGER AS $$
-#     BEGIN
-#         NEW.update_time = now();
-#         RETURN NEW;
-#     END;
-#     $$ language 'plpgsql';
+if DIALECT == "postgresql":
+    update_time_trigger = DDL(
+        '''
+        CREATE OR REPLACE FUNCTION set_update_time()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.update_time = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
 
-#     CREATE TRIGGER source_update_time
-#         BEFORE UPDATE ON source
-#         FOR EACH ROW
-#         EXECUTE PROCEDURE  set_update_time();
-#     '''
-# )
+        CREATE TRIGGER source_update_time
+            BEFORE UPDATE ON source
+            FOR EACH ROW
+            EXECUTE PROCEDURE  set_update_time();
+        '''
+    )
 
-# event.listen(Source.__table__, 'after_create', update_time_trigger)
+    event.listen(Source.__table__, 'after_create', update_time_trigger)

elsametric/models/source_metric.py
@@ -2,13 +2,16 @@ from sqlalchemy import CheckConstraint, Column, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import DECIMAL, INTEGER, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 
 
 class Source_Metric(Base):
     __tablename__ = 'source_metric'
     __table_args__ = (
-        # CheckConstraint('id >= 0', name='source_metric_id_unsigned'),
+        CheckConstraint(
+            'id >= 0',
+            name='source_metric_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
         CheckConstraint('year >= 1970 AND year <= 2069', name='year_range'),
         UniqueConstraint(
             'source_id', 'type', 'year', name='uq_sourceid_type_year'),

elsametric/models/subject.py
@@ -2,14 +2,17 @@ from sqlalchemy import CheckConstraint, Column
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import INTEGER, VARCHAR
 
-from .base import Base
+from .base import Base, DIALECT
 from .associations import Source_Subject
 
 
 class Subject(Base):
     __tablename__ = 'subject'
     __table_args__ = (
-        # CheckConstraint('id >= 0', name='subject_id_unsigned'),
+        CheckConstraint(
+            'id >= 0',
+            name='subject_id_unsigned'
+        ) if DIALECT == "postgresql" else None,
         CheckConstraint('asjc >= 0', name='subject_asjc_unsigned'),
     )
 